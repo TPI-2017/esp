d4d0cb51d1625d024839953ee7d3b93512753fbb
Ramiro <ramiroromero94@gmail.com>
2017-09-23 10:11:22 -0300

    Linefeeds

--- wifi.c
+++ wifi.c
@@ -11,16 +11,17 @@ static void wifi_cb(System_Event_t * evt);
 void ICACHE_FLASH_ATTR wifi_init(void)
 {
 	os_printf("wifi_init: Setting up 802.11 to station mode ... ");
-	if (!wifi_set_opmode_current(1)) {
+
+	if (!wifi_set_opmode_current(1))
 		os_printf("Failed to set 802.11 station mode!\n");
-	}
+
 	os_memcpy(&wifi_config.ssid, SSID, 32);
 	os_memcpy(&wifi_config.password, PASSWORD, 64);
 	wifi_config.bssid_set = 0;
 	wifi_set_event_handler_cb(wifi_cb);
-	if (!wifi_station_set_config_current(&wifi_config)) {
+
+	if (!wifi_station_set_config_current(&wifi_config))
 		os_printf("Failed to set 802.11 config!\n");
-	}
 }
 
 static void ICACHE_FLASH_ATTR wifi_cb(System_Event_t * evt)
