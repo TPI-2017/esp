cf4414a43f1f09ef623b76808f45eea3649e994b
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-11-13 20:32:23 -0300

    Muriendo haciendo get y set

--- settings.cpp
+++ settings.cpp
@@ -1,2 +1,102 @@
 #include "settings.h"
 
+void Settings::loadSettings()
+{
+	setSSID("Fibertel");
+	mWiFiPassword.write("lipschitzpotato", mWiFiPassword.capacity());
+	mIP = 0;
+	mSubnetMask = 0;
+	mPassword.write("1234", mPassword.capacity());
+	mText.write("Hello World!", mText.capacity());
+	mBlinkRate = 0;
+	mSlideRate = 0;
+}
+
+void Settings::storeSettings()
+{
+	#warning No implementado.
+}
+
+const char *Settings::ssid()
+{
+	return reinterpret_cast<const char*>(mSSID.data());
+}
+
+const char *Settings::wifiPassword()
+{
+	return reinterpret_cast<const char*>(mWiFiPassword.data());
+}
+
+uint32_t Settings::ip()
+{
+	return mIP;
+}
+
+uint32_t Settings::subnetMask()
+{
+	return mSubnetMask;
+}
+
+const char *Settings::password()
+{
+	return reinterpret_cast<const char*>(mPassword.data());
+}
+
+const char *Settings::text()
+{
+	return reinterpret_cast<const char*>(mText.data());
+}
+
+uint8_t Settings::blinkRate()
+{
+	return mBlinkRate;
+}
+
+uint16_t Settings::slideRate()
+{
+	return mSlideRate;
+}
+
+void Settings::setSSID(const char *ssid)
+{
+	mSSID.clear();
+	mSSID.write(ssid, mSSID.capacity());
+}
+
+void Settings::setWiFiPassword(const char *wifiPassword)
+{
+	mWiFiPassword.clear();
+	mWiFiPassword.write(wifiPassword, mWiFiPassword.capacity());
+}
+
+void Settings::setIP(uint32_t ip)
+{
+	mIP = ip;
+}
+
+void Settings::setSubnetMask(uint32_t subnetMask)
+{
+	mSubnetMask = subnetMask;
+}
+
+void Settings::password(const char *password)
+{
+	mPassword.clear();
+	mPassword.write(password, mPassword.capacity());
+}
+
+void Settings::setText(const char *text)
+{
+	mText.clear();
+	mText.write(text, mText.capacity());
+}
+
+void Settings::setBlinkRate(uint8_t blinkRate)
+{
+	mBlinkRate = blinkRate;
+}
+
+void Settings::setSlideRate(uint16_t slideRate)
+{
+	mSlideRate = slideRate;
+}
--- settings.h
+++ settings.h
@@ -2,18 +2,40 @@
 #define SETTINGS_H
 
 #include "protocolo/Message.h"
+#include "buffer.h"
 
 class Settings {
 public:
-	static char    ssid[Message::WIFI_SSID_SIZE + 1];
-	static char    wifiPassword[Message::WIFI_PASSWORD_SIZE + 1];
-	static char    password[Message::PASSWORD_SIZE + 1];
-	static char    text[Message::TEXT_SIZE + 1];
-	static uint8_t blinkRate;
-	static uint8_t slideRate;
-
 	static void loadSettings();
 	static void storeSettings();
+
+	static const char *ssid();
+	static const char *wifiPassword();
+	static uint32_t ip();
+	static uint32_t subnetMask();
+	static const char *password();
+	static const char *text();
+	static uint8_t blinkRate();
+	static uint16_t slideRate();
+
+	static void setSSID(const char *ssid);
+	static void setWiFiPassword(const char *wifiPassword);
+	static void setIP(uint32_t ip);
+	static void setSubnetMask(uint32_t subnetMask);
+	static void password(const char *password);
+	static void setText(const char *text);
+	static void setBlinkRate(uint8_t blinkRate);
+	static void setSlideRate(uint16_t slideRate);
+
+private:
+	static Buffer<Message::WIFI_SSID_SIZE + 1>     mSSID;
+	static Buffer<Message::WIFI_PASSWORD_SIZE + 1> mWiFiPassword;
+	static uint32_t                                mIP;
+	static uint32_t                                mSubnetMask;
+	static Buffer<Message::PASSWORD_SIZE + 1>      mPassword;
+	static Buffer<Message::TEXT_SIZE + 1>          mText;
+	static uint8_t                                 mBlinkRate;
+	static uint8_t                                 mSlideRate;
 };
 
 #endif
